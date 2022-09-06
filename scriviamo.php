61ece579f63c059c106a7a77e661bc2ce65fba80
Riccardo Mel <riky.mel@gmail.com>
2022-09-06 11:18:50 +0200

    Fix access control

--- scriviamo.php
+++ scriviamo.php
@@ -3,7 +3,7 @@
  * Plugin Name: Scriviamo AI - Wordpress Plugin
  * Plugin URI:  https://scriviamo.ai
  * Description: Scriviamo AI - Wordpress Plugin
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Scriviamo ai
  * Author URI:  https://scriviamo.ai
  * License:     GPL-3.0
@@ -16,17 +16,17 @@
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
 
- /**
+/**
  * Handle Cors
  */
 add_action('init', 'handle_preflight');
 function handle_preflight() {
     $origin = get_http_origin();
-    if ($origin === 'https://aieditoriale.lndo.site' || $origin === 'https://ai-editoriale.evolveit.agency' || $origin === 'https://scriviamo.ai' || $origin === 'https://hoppscotch.io' || $origin === 'http://localhost:3000') {
-        header("Access-Control-Allow-Origin: *");
+    if ($origin === 'https://aieditoriale.lndo.site' || $origin === 'https://ai-editoriale.evolveit.agency' || $origin === 'https://scriviamo.ai' || $origin === 'http://localhost:3000') {
+        header("Access-Control-Allow-Origin: $origin");
         header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Credentials: true");
-        header('Access-Control-Allow-Headers: Origin, X-Requested-With, X-WP-Nonce, Content-Type, Accept, Authorization');
+        header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin, X-Requested-With, X-WP-Nonce, Content-Type, Accept, Authorization');
         if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
             status_header(200);
             exit();
